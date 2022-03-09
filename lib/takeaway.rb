a8593d7f824e225bef0d04d1f8962e8f3197c196
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-09 16:59:50 +0000

    discount

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -22,19 +22,29 @@ end
 class Order
   def initialize
     @current_order = []
+    @random_number = rand(1..20)
   end
   
   def add(name, price)
     @current_order << {"name" => name, "price" => price}
   end
 
+  def random_number
+    @random_number
+  end
+
   def show_reciept
     reciept = []
     total_price = 0
-    @current_order.each {|i|
+    @current_order.each { |i|
       reciept << "#{i["name"]}: £#{i["price"]}"
-      total_price += i["price"].to_i
-  }
+      total_price += i["price"]
+    }
+    if @random_number == 1
+      discount = total_price / 2
+      reciept << "Discount: -£#{discount}"
+      total_price /= 2
+    end
     reciept << "Total: £#{total_price}"
     return reciept.join(", ")
   end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -22,4 +22,23 @@ RSpec.describe Takeaway do
     result = order.show_reciept
     expect(result).to eq "Pepperoni: £10, Margarita: £9, Total: £19"
   end 
+
+  it 'reduces bill by 50% for a random 5% of customers' do
+    takeaway = Takeaway.new
+    orders = Array.new(100) { |i| Order.new }
+    orders.each do |order|
+      takeaway.select_item(order, "Pepperoni")
+      takeaway.select_item(order, "Margarita")
+      takeaway.select_item(order, "Hawaiian")
+    end
+    orders.each do |order|
+      result = order.show_reciept
+      if order.random_number == 1
+        expect(result).to eq "Pepperoni: £10, Margarita: £9, Hawaiian: £11, Discount: -£15, Total: £15"
+      else
+        expect(result).to eq "Pepperoni: £10, Margarita: £9, Hawaiian: £11, Total: £30"
+      end
+    end
+  end
 end
+
